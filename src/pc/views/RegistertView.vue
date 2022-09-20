9d2869309f1092753c4b5e17a135fbb05f4e60e0
tail35 <ragingfire35@gmail.com>
2022-09-20 19:21:16 +0800

    add

--- src/pc/views/RegistertView.vue
+++ src/pc/views/RegistertView.vue
@@ -1,22 +1,41 @@
 <template>
   <div class="divregister">     
-     <form class="myform mytable" action="register">
+     <form id="idform" class="myform mytable" action="https://my1.cow8.cn:448/register?v=1" method="Post" enctype="multipart/form-data" name="myregister" target="frameName">
       <div class="left cell item1">*手机：</div>  <input class="right cell item2" type="text" name="phone">
       <div class="left cell item3">*密码：</div>  <input class="right cell item4" type="text" name="password">
       <div class="left cell item4">*确认：</div>  <input class="right cell item6" type="text" name="confirm">
-      <div class="left cell item7">*验证码：</div>  <input class="right cell item8" type="text" name="checkcode"> <div class="left cell item9">验证码</div>
-      <input class=" cell item10" type="submit" value="提交"> 
+      <div class="left cell item7">*验证码：</div>  <input class="right cell item8" type="text" name="checkcode"> <img id="idimg" onclick="OnClickImg()" class="left cell item9"  />
+      <input class=" cell item10" type="submit" value="提交" /> 
     </form>
   </div>
+  <iframe src="" frameborder="0" name="frameName"></iframe>
 </template>
 
+<script setup lang="ts">
+    window.onload =function(){      
+      var url= "https://my1.cow8.cn:448/register?v="    
+      var myfrom = document.getElementById("idform")      
+      myfrom.action = url+Math.random()
+
+      var img = document.getElementById("idimg")
+      img.src =url+Math.random()
+    }
+    window.OnClickImg = function OnClickImg()
+    {
+      var img = document.getElementById("idimg")
+      img.src ="https://my1.cow8.cn:448/checkcode?v="+Math.random()
+    }
+
+    import {SubmitRegister} from "../js/register"
+    SubmitRegister()
+</script>
 <style>
 .mytable{
-  display: grid;  
+  display: grid;
   grid-template-columns: auto auto;
-  grid-auto-rows : 40px;  
+  grid-auto-rows: 40px;
   width: 280px;
-  height: 200px;  
+  height: 200px;
   margin: auto;
   margin-top: 200px;
   /* border: 1px solid lightcoral; */
@@ -26,7 +45,6 @@
   text-align: right;
   vertical-align: center;
   width: 80px;
-  
 }
 .cell{
   height: 25px;
@@ -43,5 +61,11 @@
     min-height: 100vh;    
     /* border: 1px solid greenyellow; */
   }
+  
+  .item9:hover{
+    cursor: pointer;
+  }
+
+
 
 </style>
