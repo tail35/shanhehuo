2d258986c3d73403ab45be40491e9cedf84d0c5b
tail35 <ragingfire35@gmail.com>
2022-09-21 16:12:48 +0800

    parent receive child

--- src/pc/views/RegistertView.vue
+++ src/pc/views/RegistertView.vue
@@ -5,22 +5,37 @@
       <div class="left cell item3">*密码：</div>  <input class="right cell item4" type="text" name="password">
       <div class="left cell item4">*确认：</div>  <input class="right cell item6" type="text" name="confirm">
       <div class="left cell item7">*验证码：</div>  <input class="right cell item8" type="text" name="checkcode"> <img id="idimg" onclick="OnClickImg()" class="left cell item9"  />
-      <input class=" cell item10" type="submit" value="提交" /> 
+      <button class=" cell item10" type="submit" value="提交" >提交</button>
     </form>
-  </div>
-  <iframe src="" frameborder="0" name="frameName"></iframe>
+    <iframe class="iframe" id="myIframe1" name="frameName" >
+    </iframe>
+  </div>  
+  
 </template>
 
 <script setup lang="ts">
-    window.onload =function(){      
-      var url= "https://my1.cow8.cn:448/register?v="    
-      var myfrom = document.getElementById("idform")      
+    window.ContentChange = function(data)
+    {
+      console.log(data)
+    }
+    
+    window.addEventListener("message", function(e){        
+        if( e.data.action == "res"){
+          window.ContentChange(e.data)
+        }
+    },false);//跨域通信，子通知父页面.不能少false,不然收不到。
+    
+
+    window.onload =function(){
+      
+      var url= "https://my1.cow8.cn:448/register?v="
+      var myfrom = document.getElementById("idform")
       myfrom.action = url+Math.random()
 
       var img = document.getElementById("idimg")
       img.src =url+Math.random()
     }
-    window.OnClickImg = function OnClickImg()
+    window.OnClickImg = function()
     {
       var img = document.getElementById("idimg")
       img.src ="https://my1.cow8.cn:448/checkcode?v="+Math.random()
@@ -66,6 +81,12 @@
     cursor: pointer;
   }
 
-
+.iframe{  
+  frameborder:"0";
+  padding: 0;
+  margin: 0;
+  width:0;
+  height:0;
+}
 
 </style>
